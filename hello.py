44f3650e4367f71ee5a1ef3d1c3ad32ed6884635
taffer23 <taffer23@greenhill.org>
2021-09-03 13:02:40 -0500

    1st commit

---
+++ hello.py
@@ -0,0 +1,26 @@
+# this is for comments
+
+#Ryan Taffe
+#date: 09/03/2021
+#this is our first program, test average
+
+#Set variables
+test1=92
+test2=89
+test3=91
+test4=87
+
+#declare average variable
+average=0 
+print(type(average))
+
+# Four basic operators
+# # Sum =
+# sub -
+# mult *
+# div /
+average= (test1+test2+test3+test4)/4
+#let's print result
+
+#command using keywords
+print("The tests average is", average)
