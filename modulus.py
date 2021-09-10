51731260cc731b18a7191cee3b77463c68a153e3
taffer23 <taffer23@greenhill.org>
2021-09-10 13:06:21 -0500

    Todays work

---
+++ modulus.py
@@ -0,0 +1,21 @@
+#learning about operators
+#Ryan Taffe
+
+# + - / * add, sub, div, multi
+# ** exp
+# 5 mosulus reminder of division
+
+#find and even number
+
+num=int(input("give me an integer"))
+print(num)
+
+if(num %2 ==0):
+    print("the number is even")
+else:
+    print("The number is odd")
+
+num2=float(input("give me any number no more than 2 decimals"))
+print(num2)
+print(num + num2)
+print(num2%2)
